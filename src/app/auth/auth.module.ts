07f0e05621d9fc011160926d8a38e268b12e6570
Winston Sevilla <winston_sevilla@outlook.com>
2020-02-23 16:51:18 +0800

    Add UI for Login.

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,11 +3,13 @@ import { AuthRoutingModule } from './auth-route.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthComponent } from './auth.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 @NgModule({
   imports: [
     AuthRoutingModule,
-    CommonModule
+    CommonModule,
+    MDBBootstrapModule.forRoot(),
   ],
   declarations: [
     AuthComponent,

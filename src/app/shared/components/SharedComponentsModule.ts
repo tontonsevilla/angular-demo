2517749f417c7e1c5646f9ce546e013af73557a7
Winston Sevilla <winston_sevilla@outlook.com>
2020-02-29 23:07:14 +0800

    WIP - SPinner

--- src/app/shared/components/SharedComponentsModule.ts
+++ src/app/shared/components/SharedComponentsModule.ts
@@ -1,3 +1,4 @@
+import { SpinnerOverlayWrapperComponent } from './spinner-overlay-wrapper/spinner-overlay-wrapper.component';
 
 import { NgModule, Injectable } from '@angular/core';
 import { SpinnerComponent } from './spinner/spinner.component';
@@ -5,7 +6,13 @@ import { SpinnerComponent } from './spinner/spinner.component';
 @NgModule({
     imports: [
     ],
-    declarations: [SpinnerComponent],
-    exports: [SpinnerComponent]
+    declarations: [
+      SpinnerComponent,
+      SpinnerOverlayWrapperComponent
+    ],
+    exports: [
+      SpinnerComponent,
+      SpinnerOverlayWrapperComponent
+    ]
   })
   export class SharedComponentsModule { }
---
+++ src/app/shared/components/spinner-overlay-wrapper/spinner-overlay-wrapper.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-spinner-overlay-wrapper',
+  templateUrl: './spinner-overlay-wrapper.component.html',
+  styleUrls: ['./spinner-overlay-wrapper.component.css']
+})
+export class SpinnerOverlayWrapperComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
